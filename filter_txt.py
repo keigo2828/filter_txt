2de6eacbd30bbca127f4074fc988bdbb8b933abd
kei52828go@gmail.com <102941077+keigo2828@users.noreply.github.com>
2023-04-19 15:36:56 +0900

    文字列を使ってファイル仕分け

--- filter_txt.py
+++ filter_txt.py
@@ -1,6 +1,28 @@
 import shutil
 import os
+import glob
+import time
 
-directory = os.getcwd()
 
-print(directory)
+
+def move_glob(dst_path,recursive=True):
+    with open('./list.txt') as f:
+        for line in f:
+            year = line[0:2]
+            number = line[4:7]
+            P = glob.glob(f"./input_file/*{year}*{number}.txt", recursive=recursive)
+            for p in P:
+                #print(p[0],year,number)
+                shutil.move(p, dst_path)
+
+
+current_dir_path = os.getcwd()
+output_file_path = current_dir_path + '/output_file'
+input_file_path = current_dir_path + '/input_file'
+
+os.makedirs(output_file_path, exist_ok = True) 
+os.makedirs(input_file_path, exist_ok = True) 
+
+#print(glob.glob(input_file_path + '/*.txt'))
+
+move_glob(output_file_path)
